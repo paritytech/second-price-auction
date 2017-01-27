5ffe492248d534f52b95027d238cd38bc25132ce
Gav <gavin@parity.io>
2017-01-27 17:54:10 +0100

    Version updates.

--- src/client/scripts/entry.jsx
+++ src/client/scripts/entry.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {render} from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import {setDefaultTransformBondContext} from 'oo7';
 import {setupBonds} from 'oo7-parity';
+
 import {App} from './app.jsx'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
