d32abc1ed34955c7efd728e04ef19b8bce297490
keorn <pczaban@gmail.com>
2017-10-03 17:11:13 +0100

    pure -> constant

--- src/contracts/MultiCertifier.sol
+++ src/contracts/MultiCertifier.sol
@@ -20,9 +20,9 @@ contract Certifier {
 	event Confirmed(address indexed who);
 	event Revoked(address indexed who);
 	function certified(address) public constant returns (bool);
-	function get(address, string) public pure returns (bytes32) {}
-	function getAddress(address, string) public pure returns (address) {}
-	function getUint(address, string) public pure returns (uint) {}
+	function get(address, string) public constant returns (bytes32);
+	function getAddress(address, string) public constant returns (address);
+	function getUint(address, string) public constant returns (uint);
 }
 
 /**
