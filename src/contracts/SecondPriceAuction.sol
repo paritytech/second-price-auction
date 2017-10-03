7aefa7fbf4ed9dd75d28ebae3e3c14821b6d3a9d
keorn <pczaban@gmail.com>
2017-10-03 13:31:08 +0100

    curve comment

--- src/contracts/SecondPriceAuction.sol
+++ src/contracts/SecondPriceAuction.sol
@@ -20,6 +20,8 @@ contract Certifier {
 
 /// Simple modified second price auction contract. Price starts high and monotonically decreases
 /// until all tokens are sold at the current price with currently received funds.
+/// The price curve has been chosen to resemble a logarithmic curve
+/// and produce a reasonable auction timeline.
 contract SecondPriceAuction {
 	// Events:
 
